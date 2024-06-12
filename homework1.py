43b37184c4b997af19693074d0741ea62c2a3ea1
Oleg <otrishin@gmail.com>
2024-06-12 13:31:49 +1000

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example = "Топинамбур"
+print(example[0])     # Первый символ строки
+print(example[-1])    # Последний символ строки
+print(example[5:])    # Вторая половина строки
+print(example[::-1])  # Наоборот
+print(example[1::2])   # Каждый второй символ
